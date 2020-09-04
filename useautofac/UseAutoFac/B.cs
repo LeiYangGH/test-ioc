06e8af5503dabdf371602bf67e9286133191026d
LeiYangGH <675686066@qq.com>
2020-09-04 18:10:04 +0800

    b ctor

---
+++ useautofac/UseAutoFac/B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseAutoFac
+{
+    public class B
+    {
+        private IA ia;
+        public B(IA ia)
+        {
+            ia.Fa();
+            Console.WriteLine("B ctor");
+        }
+
+        public static B CreateB(IA ia)
+        {
+            return new B(ia);
+        }
+
+        public void Fb()
+        {
+            Console.WriteLine("Fb");
+        }
+    }
+}
--- useautofac/UseAutoFac/Program.cs
+++ useautofac/UseAutoFac/Program.cs
@@ -7,6 +7,7 @@ namespace UseAutoFac
     {
         private static IContainer Container { get; set; }
         static IA ia;
+        static B b;
 
         static void Main(string[] args)
         {
@@ -14,12 +15,15 @@ namespace UseAutoFac
             ContainerBuilder builder = new ContainerBuilder();
 
             builder.RegisterType<A>().As<IA>();
+            builder.RegisterType<B>().As<B>();
 
             Container = builder.Build();
             using (var scope = Container.BeginLifetimeScope())
             {
                 ia = scope.Resolve<IA>();
                 ia.Fa();
+                b = scope.Resolve<B>();
+                b.Fb();
             }
             Console.WriteLine("Hello World!");
         }
