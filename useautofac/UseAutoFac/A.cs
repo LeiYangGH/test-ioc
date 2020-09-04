ef00fc5672f365221ec44e007e428b919a3e7179
LeiYangGH <675686066@qq.com>
2020-09-04 17:59:02 +0800

    Resolve

---
+++ useautofac/UseAutoFac/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseAutoFac
+{
+    public class A : IA
+    {
+        public void Fa()
+        {
+            Console.WriteLine("Fa");
+        }
+    }
+}
--- useautofac/UseAutoFac/Program.cs
+++ useautofac/UseAutoFac/Program.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Autofac;
+using System;
 
 namespace UseAutoFac
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+        static IA ia;
+
         static void Main(string[] args)
         {
+
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<A>().As<IA>();
+
+            Container = builder.Build();
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                ia = scope.Resolve<IA>();
+                ia.Fa();
+            }
             Console.WriteLine("Hello World!");
         }
     }
